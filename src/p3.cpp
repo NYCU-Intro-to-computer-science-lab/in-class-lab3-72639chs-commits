28d06c436f3dd5fafb077886cb1475177f1ee864
72639chs-commits <72639chs@gmail.com>
2025-11-25 14:10:23 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -2,9 +2,28 @@
 
 #include <iostream>
 using namespace std;
-
-int main() {
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
     
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+    int n;
+    cin >> n;
+    
+    for (int i = 2; i <= n / 2; i++) {
+        if (isPrime(i) && isPrime(n - i)) {
+            cout << i << " " << n - i << endl;
+            break;
+        }
+    }
     
     return 0;
 }
