85f6be393fba702fa129a6a89f99373e60b15cbd
72639chs-commits <72639chs@gmail.com>
2025-11-25 14:34:26 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -4,12 +4,16 @@
 using namespace std;
 void hanoi(int n, char from, char to, char aux) {
     if (n == 1) {
-        cout << "Move ring 1 from " << from << " to " << to << endl;
+        cout << "Move ring 1 from " << from << " to " << aux << endl;
+        cout << "Move ring 1 from " << aux << " to " << to << endl;
         return;
     }
-    hanoi(n - 1, from, aux, to);
-    cout << "Move ring " << n << " from " << from << " to " << to << endl;
-    hanoi(n - 1, aux, to, from);
+    
+    hanoi(n - 1, from, to, aux);
+    cout << "Move ring " << n << " from " << from << " to " << aux << endl;
+    hanoi(n - 1, to, from, aux);
+    cout << "Move ring " << n << " from " << aux << " to " << to << endl;
+    hanoi(n - 1, from, to, aux);
 }
 
 int main() {
