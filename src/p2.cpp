ec2e1a2165942f19ac0e9cb1236279d89c81fb29
72639chs-commits <72639chs@gmail.com>
2025-11-25 14:01:28 +0800

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -3,14 +3,17 @@
 #include <iostream>
 using namespace std;
 int gcd(int a, int b) {
-    if (b == 0)
-        return a;
+    if (b == 0) return a;
     return gcd(b, a % b);
 }
-int main() {
-      int a, b;
-    while (cin >> a >> b) {
-        cout << gcd(a, b) << "\n";
+void processInput() {
+    int a, b;
+    if (cin >> a >> b) {
+        cout << gcd(a, b) << endl;
+        processInput();
     }
+}
+int main() {
+    processInput();
     return 0;
 }
