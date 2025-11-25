ec5b39bdcaa3d7a1f5f64b3ef702351cfbb9de8c
72639chs-commits <72639chs@gmail.com>
2025-11-25 14:30:26 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -2,14 +2,15 @@
 #include <cmath>
 
 using namespace std;
-void hanoi(int n, int from, int to, int aux);
+void hanoi(int n, char from, char to, char aux) {
     if (n == 1) {
-        cout << "Move disk 1 from" << from << "to" << to << endl;
-        return; 
+        cout << "Move ring 1 from " << from << " to " << to << endl;
+        return;
     }
-hanoi(n-1, from, aux, to);
-cout << "Move ring" << n << "from" << from << "to" << to << endl;
-hanoi(n-1, to, from, aux);
+    hanoi(n - 1, from, aux, to);
+    cout << "Move ring " << n << " from " << from << " to " << to << endl;
+    hanoi(n - 1, aux, to, from);
+}
 
 int main() {
     int n;
